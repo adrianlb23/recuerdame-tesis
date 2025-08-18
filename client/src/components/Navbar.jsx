ece0368ed396fd8e04a68b17f0ea04093aae9ae3
adrian <alopezbanobre@gmail.com>
2025-08-17 22:22:33 -0400

    sprint 1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,51 +1,66 @@
 import React, { useRef } from "react";
 import { Link } from "react-router-dom";
-import "../styles/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Navbar.css";
 import logo from "../assets/logo.png";
 
 export default function Navbar() {
   const navRef = useRef(null);
+  const overlayRef = useRef(null);
 
-  const abrirMenu = () => {
-    navRef.current.classList.add("visible");
+  const toggleMenu = () => {
+    navRef.current.classList.toggle("visible");
+    overlayRef.current.classList.toggle("visible");
+    document.body.classList.toggle("no-scroll");
   };
 
-  const cerrarMenu = () => {
+  const closeMenu = () => {
     navRef.current.classList.remove("visible");
+    overlayRef.current.classList.remove("visible");
+    document.body.classList.remove("no-scroll");
   };
 
   return (
-    <header className="modern-header">
-      <div
-        className="logo-container"
-        onClick={() => (window.location.href = "/")}
-        style={{ cursor: "pointer" }}
-      >
-        <img
-          className="logo-img"
-          src={logo}
-          alt="Logo"
-        />
-        <h1>Perfumería Recuérdame</h1>
-      </div>
+    <>
+      <header className="navbar">
+        <div className="navbar__logo-container" onClick={() => window.location.href = "/"}>
+          <img className="navbar__logo-img" src={logo} alt="Logo" />
+          <h1 className="navbar__title">Perfumería Recuérdame</h1>
+        </div>
 
-      <button id="abrir" className="abrir-menu" onClick={abrirMenu}>
-        <i className="fas fa-bars" />
-      </button>
-
-      <nav className="nav" id="nav" ref={navRef}>
-        <button className="cerrar-menu" id="cerrar" onClick={cerrarMenu}>
-          <i className="fas fa-times" />
+        <button className="navbar__hamburger" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
         </button>
-        <ul className="nav-list">
-          <li><Link to="/hombre">Masculino</Link></li>
-          <li><Link to="/mujer">Femenino</Link></li>
-          <li><Link to="/nicho">Nicho</Link></li>
-          <li><Link to="/precios">Precios</Link></li>
-          <li><Link to="/promociones">Novedades</Link></li>
-          <li><Link to="/recomendador">Recomendador</Link></li>
-        </ul>
-      </nav>
-    </header>
+
+        <div className="navbar__overlay" ref={overlayRef} onClick={closeMenu}></div>
+
+        <nav className="navbar__menu" ref={navRef}>
+          <button className="navbar__close-btn" onClick={closeMenu}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <Link to="/hombre" className="navbar__link" onClick={closeMenu}>Masculino</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="/mujer" className="navbar__link" onClick={closeMenu}>Femenino</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="/nicho" className="navbar__link" onClick={closeMenu}>Nicho</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="/precios" className="navbar__link" onClick={closeMenu}>Precios</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="/promociones" className="navbar__link" onClick={closeMenu}>Novedades</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="/recomendador" className="navbar__link" onClick={closeMenu}>Recomendador</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
